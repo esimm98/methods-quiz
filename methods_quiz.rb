ef6cdc4c58f82d9e16e39db7fe33b935e18e5df7
Ethan Simmons <simmonsde@s.dcsdk12.org>
2015-09-02 09:55:12 -0600

    has_teen? complete, sorry Padjen, I didnt notice the test commit message the first time

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,8 @@
 module MethodsQuiz
 	
-	# TODO - write has_teen?
+	def has_teen?(a, b, c)
+		(a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19)
+	end
 	
 	# TODO - write not_string
 	
