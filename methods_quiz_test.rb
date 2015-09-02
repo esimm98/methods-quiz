f4a700f41bc2aa93f2bd6224f037b96dfd4bca89
Ethan Simmons <simmonsde@s.dcsdk12.org>
2015-09-02 09:49:52 -0600

    Has teen? test written

--- methods_quiz_test.rb
+++ methods_quiz_test.rb
@@ -11,6 +11,12 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
-	# TODO - write tests here
+	def test_has_teen
+		assert_equal true, @m.has_teen?(11, 12, 13)
+		assert_equal true, @m.has_teen?(60, 18, 10)
+		assert_equal true, @m.has_teen?(9, 5, 19)
+		assert_equal false, @m.has_teen?(1, 2, 3)
+		assert_equal false, @m.has_teen?(10, 11, 12)
+	end
 
 end
