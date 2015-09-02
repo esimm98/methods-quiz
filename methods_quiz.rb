74dca8dc7d82d34b9a191e8edc1163f95fa4b891
Ethan Simmons <simmonsde@s.dcsdk12.org>
2015-09-02 10:05:11 -0600

    not_string complete

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -4,7 +4,13 @@ module MethodsQuiz
 		(a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19)
 	end
 	
-	# TODO - write not_string
+	def not_string(str)
+		if str[0] == "n" && str[1] == "o" && str[2] == "t"
+			return str
+		else
+			return "not " + str
+		end
+	end
 	
 	# TODO - write icy_hot?
 	
